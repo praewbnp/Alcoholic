b60bdd108e022f6b1b49d5d38ab1db2d29417156
preawbnp <budnampetch.o@ku.th>
2017-05-27 23:45:04 +0700

    random orderList

--- app/src/main/java/com/example/preawbnp/alcoholic/MainActivity.java
+++ app/src/main/java/com/example/preawbnp/alcoholic/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.preawbnp.alcoholic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String[] orderList = {"A","B","C","D","E"};
+        final TextView randomText = (TextView) findViewById(R.id.textRandom);
+
+        Button randomBtn = (Button) findViewById(R.id.btnRandom);
+
+        randomBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Random randomNext = new Random();
+                int index = randomNext.nextInt(5);
+                randomText.setText(orderList[index]);
+            }
+        });
+
     }
 }
