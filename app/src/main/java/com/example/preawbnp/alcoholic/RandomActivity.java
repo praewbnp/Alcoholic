e0f29268861337cbe430f8edd1366ec134b476ea
preawbnp <budnampetch.o@ku.th>
2017-05-28 02:03:49 +0700

    changed orderList data

--- app/src/main/java/com/example/preawbnp/alcoholic/RandomActivity.java
+++ app/src/main/java/com/example/preawbnp/alcoholic/RandomActivity.java
@@ -6,18 +6,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomActivity extends AppCompatActivity {
+    private int balance = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random);
 
-        final String[] orderList = {"A","B","C","D","E"};
+        final String[] orderList = {"ซดโค้กครึ่งแก้ว","จ่ายค่าเหล้า 20 บาท","เพียว 1 ฝา","รอดตัวไป","คนข้างขวาโดน 1 ช็อต",
+                                    "คนข้างซ้ายโดน 1 ช็อต", "เพียว 5 วินาที", "จ่ายค่าเหล้า 100 บาท", "หมดแก้วทั้งวง", "ได้พัก 2 ตา",
+                                    "จูบโต๊ะสองที","ให้คนข้างขวาสั่งอะไรก็ได้","ให้คนข้างซ้ายสั่งอะไรก็ได้","ยกเพียว 10 วินาที","เลือกคนมากินแทน 1 คน",
+                                    "BATTLE กับคนตรงข้าม", "ซดน้ำเปล่า 1 แก้ว", "ซดโค้ก 1 แก้ว", "ข้ามคนต่อไปไป", "ย้อนกลับไปทางเดิม"};
         final TextView randomText = (TextView) findViewById(R.id.textRandom);
-
         Button randomBtn = (Button) findViewById(R.id.btnRandom);
 
         randomBtn.setOnClickListener(new View.OnClickListener() {
@@ -25,9 +29,10 @@ public class RandomActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Random randomNext = new Random();
-                int index = randomNext.nextInt(5);
+                int index = randomNext.nextInt(20);
 
-                randomText.setText(orderList[index]);
+                randomText.setText(orderList[index] + " : " + index);
+
 
             }
         });
