3736ec84fd0c9c837e5263856279beeedadbb7fb
preawbnp <budnampetch.o@ku.th>
2017-06-01 02:42:27 +0700

    update calories and common purse

--- app/src/main/java/com/example/preawbnp/alcoholic/RandomActivity.java
+++ app/src/main/java/com/example/preawbnp/alcoholic/RandomActivity.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomActivity extends AppCompatActivity {
+    private int giveup = 0;
+    private int cal = 0;
     private int balance = 0;
 
     @Override
@@ -21,8 +23,13 @@ public class RandomActivity extends AppCompatActivity {
                                     "คนข้างซ้ายโดน 1 ช็อต", "เพียว 5 วินาที", "จ่ายค่าเหล้า 100 บาท", "หมดแก้วทั้งวง", "ได้พัก 2 ตา",
                                     "จูบโต๊ะสองที","ให้คนข้างขวาสั่งอะไรก็ได้","ให้คนข้างซ้ายสั่งอะไรก็ได้","ยกเพียว 10 วินาที","เลือกคนมากินแทน 1 คน",
                                     "BATTLE กับคนตรงข้าม", "ซดน้ำเปล่า 1 แก้ว", "ซดโค้ก 1 แก้ว", "ข้ามคนต่อไปไป", "ย้อนกลับไปทางเดิม"};
+        final TextView balanceText = (TextView) findViewById(R.id.textBalance);
+        final TextView caloriesText = (TextView) findViewById(R.id.textCalories);
         final TextView randomText = (TextView) findViewById(R.id.textRandom);
+
         Button randomBtn = (Button) findViewById(R.id.btnRandom);
+        Button giveupBtn = (Button) findViewById(R.id.btnGiveUp);
+        Button startBtn = (Button) findViewById(R.id.btnStart);
 
         randomBtn.setOnClickListener(new View.OnClickListener() {
 
@@ -32,8 +39,40 @@ public class RandomActivity extends AppCompatActivity {
                 int index = randomNext.nextInt(20);
 
                 randomText.setText(orderList[index] + " : " + index);
+                switch (index) {
+                    case 0:
+                        cal += 50;
+                        break;
+                    case 1:
+                        balance += 20;
+                        break;
+                    case 2:
+                        cal += 65;
+                        break;
+                    case 4:
+                        cal += 65;
+                        break;
+                }
+                balanceText.setText("COMMON FURSE: " + balance);
+                caloriesText.setText("CALORIES: " + cal);
+//                if (orderList[index].equals("\"เพียว 5 วินาที\"")) {
+//                    final Button startBtn = (Button) findViewById(R.id.btnStart);
+//                    startBtn.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            startBtn.setText("5");
+//                        }
+//                    });
+//                }
 
+            }
+        });
 
+        giveupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                giveup++;
+                balanceText.setText("COMMON FURSE: " + (giveup * 50));
             }
         });
     }
